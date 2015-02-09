2dff27c30825506504f783f4e086c36ad9dde984
Alex Chapman <alex@farfromthere.net>
2015-02-09 11:05:52 +1100

    Working, though slightly buggy. Also, should the user's own contribution be counted when working out their own contribution?

--- RSPP.hs
+++ RSPP.hs
@@ -7,56 +7,39 @@ newtype Money = Money { unMoney :: Centi } deriving (Read, Show, Eq, Ord, Num, F
 
 data PledgeClause = FixedPledge Money
                   | PledgeAbove { paAbove :: Money, paPerUnit :: Money, paUnit :: Money }
+                  deriving (Read, Show, Eq)
 
-data Pledge = Pledge { pClauses :: [PledgeClause], pLimit :: Money }
+data Pledge = Pledge { pClauses :: [PledgeClause], pLimit :: Money } deriving (Read, Show, Eq)
 
 evalClause :: Money -> PledgeClause -> Money
 evalClause     _ (FixedPledge x)                  = x
-evalClause total (PledgeAbove above perUnit unit) = ((total - above) * perUnit) / unit
+evalClause total (PledgeAbove above perUnit unit) = max (Money 0.00) $ ((total - above) * perUnit) / unit
 
+-- Note: a pledge is evaluated as though its own contribution has already been added to the total!
 evalPledge :: Money -> Pledge -> Money
 evalPledge total (Pledge clauses limit) = min limit (sum $ map (evalClause total) clauses)
 
-maxAllPledges :: [Pledge] -> Money
-maxAllPledges = sum . map pLimit
+maxPledge :: Pledge -> Money
+maxPledge = pLimit
+
+maxPledges :: [Pledge] -> Money
+maxPledges = sum . map maxPledge
+
+minPledge :: Pledge -> Money
+minPledge = evalPledge $ Money 0.00
+
+minPledges :: [Pledge] -> Money
+minPledges = sum . map minPledge
 
 evalAllPledges :: Money -> [Pledge] -> Money
 evalAllPledges total pledges = sum $ map (evalPledge total) pledges
 
 solveInRange :: Money -> Money -> [Pledge] -> Money
-solveInRange l h pledges = if l == h then h
+solveInRange l h pledges = if (h - l) <= (Money 0.01) then h
                            else let mid      = l + ((h - l) / 2)
                                     totalMid = evalAllPledges mid pledges
                                 in if totalMid < mid then solveInRange l mid pledges
                                    else solveInRange mid h pledges
 
 solve :: [Pledge] -> Money
-solve pledges = solveInRange 0.00 (maxAllPledges pledges) pledges
-
-{-
--- A type wrapping an underlying money type, with a phantom type
-newtype Money t m = Money { unMoney :: m }
-
--- Phantom types for use with money
-data FixedAmount
-data AmountAbove
-data AmountPerUnit
-data Unit
-data Limit
-data PledgeClauseTotal
-data PledgeTotal
-data TotalRaised
-
-data PledgeClause m = FixedPledge (Money FixedAmount m)
-                    | PledgeAbove (Money AmountAbove m) (Money AmountPerUnit m) (Money Unit m)
-
-data Pledge m = Pledge [PledgeClause m] (Money Limit m)
-
-evalClause :: Fractional m => Money TotalRaised m -> PledgeClause m -> Money PledgeClauseTotal m
-evalClause _ (FixedPledge x) = Money . unMoney $ x
-evalClause (Money t) (PledgeAbove (Money above) (Money perUnit) (Money unit))
-  = Money $ ((t - above) * perUnit) / unit
-
-evalPledge :: Pledge m -> Money TotalRaised m -> Money PledgeTotal m
-evalPledge (Pledge pcs (Money limit)) t = Money $ min (sum $ map (evalClause t) pcs) limit
--}
+solve pledges = solveInRange (minPledges pledges) (maxPledges pledges) pledges
