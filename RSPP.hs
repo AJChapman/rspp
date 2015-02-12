d48959a90c3411558728f3a12ec8c401bcd9768b
Alex Chapman <alex@farfromthere.net>
2015-02-12 12:35:15 +1100

    Allow solve to work for any Foldable collection of Pledges. And rename a few things.

--- RSPP.hs
+++ RSPP.hs
@@ -1,47 +1,53 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module RSPP where
+
+import Data.Foldable as F
 
 -- The 'c' typeclass is for your currency datatype, ie. 'Centi'
 
 data PledgeClause c = FixedPledge c
                   | PledgeAbove { paAbove :: c, paPerUnit :: c, paUnit :: c }
-                  deriving (Read, Show, Eq)
+                  deriving (Eq, Read, Show)
 
-data Pledge c = Pledge { pClauses :: [PledgeClause c], pLimit :: c } deriving (Read, Show, Eq)
+data Pledge c = Pledge { pClauses :: [PledgeClause c], pLimit :: c } deriving (Eq, Read, Show)
 
 evalClause :: (Ord c, Fractional c) => c -> PledgeClause c -> c
 evalClause     _ (FixedPledge x)                  = x
 evalClause total (PledgeAbove above perUnit unit) = max (fromInteger 0) $ ((total - above) * perUnit) / unit
 
+mapSum :: (Foldable t, Num c) => (a -> c) -> t a -> c
+mapSum f p = F.foldr step (fromInteger 0) p
+    where step a sum = f a + sum
+
 -- Note: a pledge is evaluated as though its own contribution has already been added to the total!
 evalPledge :: (Ord c, Fractional c) => c -> Pledge c -> c
-evalPledge total (Pledge clauses limit) = min limit (sum $ map (evalClause total) clauses)
+evalPledge total (Pledge clauses limit) =
+    min limit (mapSum (evalClause total) clauses)
 
-maxPledge :: Pledge c -> c
-maxPledge = pLimit
+pledgeMax :: Pledge c -> c
+pledgeMax = pLimit
 
-maxPledges :: Num c => [Pledge c] -> c
-maxPledges = sum . map maxPledge
+maxPledges :: (Foldable t, Num c) => t (Pledge c) -> c
+maxPledges = mapSum pledgeMax
 
-minPledge :: (Ord c, Fractional c) => Pledge c -> c
-minPledge = evalPledge $ fromInteger 0
+pledgeMin :: (Ord c, Fractional c) => Pledge c -> c
+pledgeMin = evalPledge $ fromInteger 0
 
-minPledges :: (Ord c, Fractional c) => [Pledge c] -> c
-minPledges = sum . map minPledge
+minPledges :: (Foldable t, Ord c, Fractional c) => t (Pledge c) -> c
+minPledges = mapSum pledgeMin
 
-evalAllPledges :: (Ord c, Fractional c) => c -> [Pledge c] -> c
-evalAllPledges total pledges = sum $ map (evalPledge total) pledges
+evalPledges :: (Foldable t, Ord c, Fractional c) => c -> t (Pledge c) -> c
+evalPledges total pledges = mapSum (evalPledge total) pledges
 
-solveInRange :: (Ord c, Fractional c) => c -> c -> [Pledge c] -> c
+solveInRange :: (Foldable t, Ord c, Fractional c) => c -> c -> t (Pledge c) -> c
 solveInRange l h pledges
     | h == l    = h
     | otherwise =
         let mid = l + ((h - l) / 2)
         in  if l == mid
                then l
-               else let totalMid = evalAllPledges mid pledges
+               else let totalMid = evalPledges mid pledges
                     in  if totalMid < mid then solveInRange l mid pledges
                                           else solveInRange mid h pledges
 
-solve :: (Ord c, Fractional c) => [Pledge c] -> c
+solve :: (Foldable t, Ord c, Fractional c) => t (Pledge c) -> c
 solve pledges = solveInRange (minPledges pledges) (maxPledges pledges) pledges
